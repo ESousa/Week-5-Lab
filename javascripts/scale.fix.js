198ace95b48ad61cb935d4b107fee57ac9e16e04
Eric Sousa <e_sousa@email.neit.edu>
2013-08-21 22:10:34 -0400

    Update scale.fix.js

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -1,17 +1,100 @@
-var metas = document.getElementsByTagName('meta');
-var i;
-if (navigator.userAgent.match(/iPhone/i)) {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
-    }
-  }
-  document.addEventListener("gesturestart", gestureStart, false);
-}
-function gestureStart() {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
-    }
-  }
-}
+
+
+
+
+
+
+					$("#nav1,#nav2,#nav3,#nav4").addClass("contentBox");
+
+
+
+
+
+
+
+
+
+
+
+
+					$("#nav li").mouseover(function(){
+					$(this).animate({marginTop: "-10px"},500);
+					});$("#nav li").mouseleave(function(){
+					$(this).animate({marginTop: "0px"},500);
+					});
+
+
+
+
+					//animation nav1button
+					function changeNav1() {
+					$("#nav1").show(500);
+					$("#nav2").hide(500);
+					$("#nav3").hide(500);
+					$("#nav4").hide(500);
+
+
+
+
+					}
+					$("#nav1button").on("click",changeNav1);
+
+
+
+
+
+
+
+
+
+
+
+
+					//animation nav2button
+					function changeNav2() {
+					$("#nav1").hide(500);
+					$("#nav2").show(500);
+					$("#nav3").hide(500);
+					$("#nav4").hide(500);
+
+
+
+
+					}
+					$("#nav2button").on("click",changeNav2);
+
+
+
+
+					//animation nav3button
+					function changeNav3() {
+					$("#nav1").hide(500);
+					$("#nav2").hide(500);
+					$("#nav3").show(500);
+					$("#nav4").hide(500);
+
+
+
+
+					}
+					$("#nav3button").on("click",changeNav3);
+
+
+
+
+
+
+
+
+					//animation nav4button
+					function changeNav4() {
+					$("#nav1").hide(500);
+					$("#nav2").hide(500);
+					$("#nav3").hide(500);
+					$("#nav4").show(500);
+
+
+
+
+					}
+					$("#nav4button").on("click",changeNav4);
